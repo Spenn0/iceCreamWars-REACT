a57e87ba3748c421f60be896bcd94d2c68f6df81
Spence <spence@Spences-MacBook-Pro.local>
2023-01-19 16:05:17 -0600

    second commit

--- react-app1/src/Votes.tsx
+++ react-app1/src/Votes.tsx
@@ -10,74 +10,71 @@ function Votes(){
     const [chocolateVote, setChocolateVote] = useState(0);
     const [vanillaVote, setVanillaVote] = useState(0);
     const [strawberryVote, setStrawberryVote] = useState(0);
-    const [totalVote, setTotalVote] = useState(0)
 
     // const [iceCreamType, setIceCreamType] = useState("Strawberry");
-
-    let totalVotes = 0;
-    let chocoVotes = 0;
-    let vanillaVotes = 0;
-    let strawbVotes = 0;
+    const total = chocolateVote + vanillaVote + strawberryVote
 
 
 
 
-    const chocoClick = () => {
-        totalVotes++;
-        chocoVotes++
-        setChocolateVote(chocoVotes)
-        setTotalVote(totalVotes)
-        // setIceCreamType("Chocolate");
-        console.log(totalVotes)
-        console.log(chocoVotes)
-        console.log(chocolateVote)
+    // const chocoClick = () => {
+    //     totalVotes++;
+    //     chocoVotes++
+    //     setChocolateVote(chocoVotes + 1)
+    //     setTotalVote(totalVotes + 1)
+    //     // setIceCreamType("Chocolate");
+    //     console.log(totalVotes)
+    //     console.log(chocoVotes)
+    //     console.log(chocolateVote)
+    // }
+    // const vanillaClick = () => {
+    //     totalVotes++;
+    //     vanillaVotes++
+    //     setVanillaVote(vanillaVotes)
+    //     setTotalVote(totalVotes)
+    //     // setIceCreamType("Vanilla");
+    // }
+    // const strawbClick = () => {
+    //     totalVotes++;
+    //     strawbVotes++;
+    //     setStrawberryVote(strawbVotes)
+    //     setTotalVote(totalVotes)
+    //     // setIceCreamType("Strawberry");
+    // }
+
+
+
+    const percentages = {
+        chocolate: percentage(chocolateVote, total),
+        vanilla: percentage(vanillaVote, total),
+        strawberry: percentage(strawberryVote, total)
     }
-    const vanillaClick = () => {
-        totalVotes++;
-        vanillaVotes++
-        setVanillaVote(vanillaVotes)
-        setTotalVote(totalVotes)
-        // setIceCreamType("Vanilla");
-    }
-    const strawbClick = () => {
-        totalVotes++;
-        strawbVotes++;
-        setStrawberryVote(strawbVotes)
-        setTotalVote(totalVotes)
-        // setIceCreamType("Strawberry");
-    }
 
-
-    const graphWidth = {
-        chocoWidth:{
-            width: chocoVotes/totalVotes + "%"
-        },
-        vanillaWidth:{
-            width: vanillaVotes/totalVotes + "%"
-        },
-        strawbWidth:{
-            width: strawbVotes/totalVotes + "%"
-        }
-
-
-    }
+    
 
     return(
         <div className="voteSection">
             <div className="whatToSupport">
                 <h3>What To Support</h3>
                 {/* <Button iceCreamType="Chocolate"></Button> */}
-                <button onClick={chocoClick}>Chocolate</button>
-                <button onClick={vanillaClick}>Vanilla</button>
-                <button onClick={strawbClick}>Strawberry</button>
+                <button onClick={() => setChocolateVote(prev => prev + 1)}>Chocolate</button>
+                <button onClick={() => setVanillaVote(prev => prev + 1)}>Vanilla</button>
+                <button onClick={() => setStrawberryVote(prev => prev + 1)}>Strawberry</button>
             </div>
             <div className="graphSection">
-                <div className="chocolateGraph" style={graphWidth.chocoWidth}><span>chocolate</span><span>{chocolateVote} ({chocolateVote/totalVote * 100}%)</span></div>
-                <div className="vanillaGraph" style={graphWidth.vanillaWidth}><span>vanilla</span><span>{vanillaVote} ({vanillaVote/totalVote * 100}%)</span></div>
-                <div className="strawberryGraph" style={graphWidth.strawbWidth}><span>strawberry</span><span>{strawberryVote} ({strawberryVote/totalVote * 100}%)</span></div>
+                <div className="chocolateGraph" style={{width: percentages.chocolate}}><span>chocolate</span><span>{chocolateVote}({percentages.chocolate})</span></div>
+                <div className="vanillaGraph" style={{width: percentages.vanilla}}><span>vanilla</span><span>{vanillaVote}({percentages.vanilla})</span></div>
+                <div className="strawberryGraph" style={{width: percentages.strawberry}}><span>strawberry</span><span>{strawberryVote}({percentages.strawberry})</span></div>
             </div>
         </div>
     )
+}
+
+function percentage(n:number, total:number): string {
+    if(total === 0){
+        return "0.0%"
+    }
+    return((n/total)*100).toFixed(1) + "%"
 }
 
 export default Votes
